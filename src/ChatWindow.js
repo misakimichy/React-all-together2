03a9bc5db378293427c9da5fa22f2e4221b0a38d
Misaki Koonce <misaki.koonce@gmail.com>
2019-05-29 19:18:03 -0700

    Add prop-types.

--- src/ChatWindow.js
+++ src/ChatWindow.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class ChatWindow extends Component {
+    onMessage = message => {
+        this.props.onPostMessage(this.props.user.username, message);
+    };
+
     render() {
+        // console.log('Props', this.props);
+        const { user, messages } = this.props;
+        return (
+            <div className="chat-window">
+                <h2>Super Awesome Chat</h2>
+                <div className="name sender">{user.username}</div>
+                <MessageHistory />
+                <AddMessage />
+            </div>
+        );
     }
 }
 
-// Add Proptypes
+// Add Prop-types
+ChatWindow.propTypes = {
+    user: PropTypes.object.isRequired,
+    messages: PropTypes.array.isRequired,
+    onMessage: PropTypes.func.isRequired,
+}
 
 export default ChatWindow;
