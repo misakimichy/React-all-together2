ef43aec55dac563743fcb1730d8c5c2317e0b936
Misaki Koonce <misaki.koonce@gmail.com>
2019-05-30 15:59:37 -0700

    Remove codes called in components.

--- src/App.js
+++ src/App.js
@@ -11,20 +11,12 @@ class App extends Component {
 
   onMessage = (username, message) => {
     const newMessage = {
-      [username]: username,
-      [message]: message,
+      ['username']: username,
+      ['text']: message,
     };
     this.setState(currentState => ({
       messages: [currentState.messages.concat([newMessage])],
     }));
-  };
-
-  /*
-  If the user did not type anything, he/she should not be
-  allowed to submit.
-  */
-  isDisabled = () => {
-    return false;
   };
 
   render() {
@@ -45,59 +37,6 @@ class App extends Component {
             onMessage={this.onMessage}
           />
           ))}
-
-          {/* <ul className="message-list">
-              {messages.map((message, index) => (
-                <li
-                  key={index}
-                  className={
-                    message.username === users[0].username ? 'message sender' : 'message recipient'
-                  }
-                >
-                  <p>{`${message.username}: ${message.text}`}</p>
-                </li>
-              ))}
-            </ul>
-
-            <div>
-              <form className="input-group">
-                <input type="text" className="form-control" placeholder="Enter your message..." />
-                <div className="input-group-append">
-                  <button className="btn submit-button" disabled={this.isDisabled()}>
-                    SEND
-                  </button>
-                </div>
-              </form>
-            </div>
-          </div>
-
-          <div className="chat-window">
-            <h2>Super Awesome Chat</h2>
-            <div className="name sender">{users[1].username}</div>
-            <ul className="message-list">
-              {messages.map((message, index) => (
-                <li
-                  key={index}
-                  className={
-                    message.username === users[1].username ? 'message sender' : 'message recipient'
-                  }
-                >
-                  <p>{`${message.username}: ${message.text}`}</p>
-                </li>
-              ))}
-            </ul>
-
-            <div>
-              <form className="input-group">
-                <input type="text" className="form-control" placeholder="Enter your message..." />
-                <div className="input-group-append">
-                  <button className="btn submit-button" disabled={this.isDisabled()}>
-                    SEND
-                  </button>
-                </div>
-              </form>
-            </div>
-          </div>
         </div>
       </div>
     );
